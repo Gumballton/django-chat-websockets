da454081f676219703fe0aaa0d2a6c78a7383dea
Gumballton <pvova6620@gmail.com>
2024-05-22 22:44:20 +0300

    New field date_message

--- chatsite/chatapp/models.py
+++ chatsite/chatapp/models.py
@@ -13,6 +13,7 @@ class MessagesModel(models.Model):
     room_name = models.ForeignKey(RoomModel, on_delete=models.CASCADE)
     sender = models.CharField(max_length=255)
     message = models.TextField()
+    date_message = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
         return str(self.room_name)
