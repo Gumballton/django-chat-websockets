40ad25497abbe5fbbdd11d0d95d3b1ab0ae02f36
Gumballton <pvova6620@gmail.com>
2024-05-24 19:02:12 +0300

    changed for auth

--- chatsite/chatapp/urls.py
+++ chatsite/chatapp/urls.py
@@ -5,5 +5,5 @@ app_name = 'chatapp'
 
 urlpatterns = [
     path('', views.create_room, name='create-room'),
-    path('<str:room_name>/<str:username>/', views.message_view, name='room'),
+    path('<str:room_name>/', views.message_view, name='room'),
 ]
