9a4adebd50e0bf0848e08ec9a9608d6409567a10
Gumballton <pvova6620@gmail.com>
2024-05-25 23:52:45 +0300

    Edited and updated

--- chatsite/chatapp/urls.py
+++ chatsite/chatapp/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'chatapp'
 
 urlpatterns = [
-    path('', views.create_room, name='create-room'),
+    path('', views.MainPageView.as_view(), name='mainpage'),
+    path('create/', views.create_room, name='create-room'),
+    path('enter/', views.enter_room, name='enter-room'),
     path('<str:room_name>/', views.message_view, name='room'),
 ]
