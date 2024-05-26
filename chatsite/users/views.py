6bf05b1e8adac49a46e175993e2b1babdf84f437
Gumballton <pvova6620@gmail.com>
2024-05-26 22:17:03 +0300

    addeted user chats view and url

--- chatsite/users/views.py
+++ chatsite/users/views.py
@@ -1,10 +1,14 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import redirect, render
 
 from django.contrib.auth.views import LoginView
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
 from django.contrib.auth import logout
+from django.contrib.auth import get_user_model
 
+
+from chatapp.models import RoomModel
 from users.forms import RegisterUserForm, UserLoginForm
 # Create your views here.
 
@@ -26,3 +30,15 @@ class RegisterUser(CreateView):
 def logout_user(request):
     logout(request)
     return redirect('users:login')
+
+
+class UserProfileView(ListView):
+    model = get_user_model()
+    template_name = 'chatapp/mainpage.html'
+    context_object_name = 'chats'
+    extra_context = {'title': 'User Profile'}
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = RoomModel.objects.filter(owner=user) | RoomModel.objects.filter(members=user)
+        return queryset.distinct()
