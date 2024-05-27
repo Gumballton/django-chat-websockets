4c41b1679a612c523338ded3bea415b69f41ba4f
Gumballton <pvova6620@gmail.com>
2024-05-27 23:19:35 +0300

    Edited urls and views

--- chatsite/chatapp/views.py
+++ chatsite/chatapp/views.py
@@ -1,6 +1,7 @@
 from typing import Any
 from django.db.models.query import QuerySet
-from django.shortcuts import redirect, render
+from django.http import HttpResponseNotAllowed
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
@@ -23,6 +24,7 @@ def create_room(request):
             room = form.save(commit=False)
             room.owner = request.user
             room.save()
+            room.members.add(request.user)
             return redirect('chatapp:room', room_name=room.room_name)
     else:
         form = RoomCreateForm()
@@ -44,13 +46,28 @@ def enter_room(request):
     return render(request, 'chatapp/index.html', {'form': form, 'title': 'Enter Room'})
 
 @login_required
+def delete_chat(request, room_id):
+    room = get_object_or_404(RoomModel, pk=room_id)
+    if request.method == 'POST':
+        room.delete()
+        messages.success(request, 'Chat deleted successfully.')
+        return redirect('chatapp:mainpage')
+    else:
+        return HttpResponseNotAllowed(['POST'])
+
+@login_required
 def message_view(request, room_name):
     get_room = RoomModel.objects.get(room_name=room_name)
     get_messages = MessagesModel.objects.filter(room_name=get_room)
 
-    if request.user not in get_room.members.all():
-        get_room.members.add(request.user)
-        messages.info(request, "You have been added to the chat.")
+    if get_room.is_closed:
+        if request.user not in get_room.members.all() and request.user != get_room.owner:
+            messages.info(request, "You need an invitation to join this chat.")
+            return redirect('chatapp:mainpage')
+    else:
+        if request.user not in get_room.members.all():
+            get_room.members.add(request.user)
+            messages.info(request, "You have been added to the chat.")
 
     context =  {
         'room': get_room,
