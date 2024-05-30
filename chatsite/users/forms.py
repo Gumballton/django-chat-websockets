133a6cdcfb945e7bff8b347754c66e0d71eb9ae6
Gumballton <pvova6620@gmail.com>
2024-05-30 11:20:14 +0300

    added annotations and docstrings

--- chatsite/users/forms.py
+++ chatsite/users/forms.py
@@ -7,12 +7,36 @@ from chatapp.models import RoomModel
 
 
 class UserLoginForm(AuthenticationForm):
+
+    """
+    Form for user login.
+
+    Inherits from Django's AuthenticationForm.
+
+    Attributes:
+        model: Model for the user.
+        fields: Fields to be displayed in the form.
+    """
+
     class Meta:
         model = get_user_model()
         fields = ('username', 'password')
 
 
 class RegisterUserForm(UserCreationForm):
+
+    """
+    Form for user registration.
+
+    Inherits from Django's UserCreationForm.
+
+    Attributes:
+        username: Field for user's login name.
+        password1: Field for user's password.
+        password2: Field for confirming the password.
+        email: Field for user's email.
+    """
+
     username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
@@ -24,7 +48,15 @@ class RegisterUserForm(UserCreationForm):
         widgets = {'email': forms.TextInput(attrs={'class': 'form-input'})}
 
 
-    def clean_email(self):
+    def clean_email(self) -> str:
+
+        """
+        Clean email field and validate uniqueness.
+
+        Returns:
+            str: Cleaned email.
+        """
+
         email = self.cleaned_data['email']
         if get_user_model().objects.filter(email=email).exists():
             raise forms.ValidationError('This E-mail already exists!')
@@ -33,9 +65,23 @@ class RegisterUserForm(UserCreationForm):
     
 
 class AddMemberFrom(forms.Form):
+
+    """
+    Form for adding a member to a chat room.
+
+    Attributes:
+        user: Field for entering the username of the user to be added.
+    """
+
     user = forms.CharField(label='Username')
 
-    def clean_user(self):
+    def clean_user(self) -> get_user_model:
+        """
+        Clean user field and validate existence.
+
+        Returns:
+            User: User object.
+        """
         username = self.cleaned_data['user']
         User = get_user_model()
         if not User.objects.filter(username=username).exists():
@@ -45,6 +91,18 @@ class AddMemberFrom(forms.Form):
     
 
 class ChangePasswordForm(PasswordChangeForm):
+
+    """
+    Form for changing user's password.
+
+    Inherits from Django's PasswordChangeForm.
+
+    Attributes:
+        old_password: Field for entering the old password.
+        new_password1: Field for entering the new password.
+        new_password2: Field for confirming the new password.
+    """
+
     old_password = forms.CharField(label='Old password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
     new_password1 = forms.CharField(label='New password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
--- chatsite/users/views.py
+++ chatsite/users/views.py
@@ -1,5 +1,7 @@
+from typing import Any, Dict
+from django import forms
 from django.db.models.query import QuerySet
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 
 from django.contrib import messages
@@ -19,6 +21,19 @@ from users.forms import AddMemberFrom, ChangePasswordForm, RegisterUserForm, Use
 
 
 class UserLoginView(LoginView):
+
+    """
+    View for user login.
+
+    Inherits from Django's LoginView.
+
+    Attributes:
+        form_class: Form class for user login.
+        template_name: Template for rendering login page.
+        extra_context: Extra context data for rendering template.
+        success_url: URL to redirect to after successful login.
+    """
+
     form_class = UserLoginForm
     template_name = 'users/login.html'
     extra_context = {'title': 'Login'}
@@ -26,17 +41,53 @@ class UserLoginView(LoginView):
 
 
 class RegisterUser(CreateView):
+
+    """
+    View for user registration.
+
+    Inherits from Django's CreateView
+
+    Attributes:
+        form_class: Form class for user registration.
+        template_name: Template for rendering registration page.
+        extra_context: Extra context data for rendering template.
+        success_url: URL to redirect to after successful registration.
+    """
+
     form_class = RegisterUserForm
     template_name = 'users/register_or_login.html'
     extra_context = {'title': 'Registration'}
     success_url = reverse_lazy('users:login')
 
 
-def logout_user(request):
+def logout_user(request: HttpRequest) -> HttpResponseRedirect:
+    """
+    View for user logout.
+
+    Logs out the user and redirects to login page.
+
+    Args:
+        request: HttpRequest object.
+
+    Returns:
+        HttpResponseRedirect: Redirects to login page.
+    """
     logout(request)
     return redirect('users:login')
 
-def leave_chat(request, room_id):
+def leave_chat(request: HttpRequest, room_id: int) -> HttpResponseRedirect:
+    """
+    View for leaving a chat room.
+
+    Removes the user from the specified chat room and redirects to main page.
+
+    Args:
+        request: HttpRequest object.
+        room_id: ID of the chat room.
+
+    Returns:
+        HttpResponseRedirect: Redirects to main page.
+    """
     room = get_object_or_404(RoomModel, id=room_id)
 
     if request.user in room.members.all():
@@ -45,29 +96,82 @@ def leave_chat(request, room_id):
     return redirect('chatapp:mainpage')
 
 class UserProfileView(ListView):
+
+    """
+    View for profile.
+
+    Inherits from Django's ListView
+
+    Attributes:
+        model: Model for the user.
+        template_name: Template for rendering user profile page.
+        context_object_name: Name of the variable to use in the template for the list of objects.
+        extra_context: Extra context data for rendering template.
+        paginate_by: Number of objects to display per page.
+    """
+
     model = get_user_model()
     template_name = 'chatapp/mainpage.html'
     context_object_name = 'chats'
     extra_context = {'title': 'User Profile'}
     paginate_by = 8
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
+        """
+        Get queryset for user profile.
+
+        Returns:
+            QuerySet: QuerySet of chat rooms where the user is either owner or member.
+        """
         user = self.request.user
         queryset = RoomModel.objects.filter(owner=user) | RoomModel.objects.filter(members=user)
         return queryset.distinct()
     
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> Dict[str, Any]:
+        """
+        Get extra context data.
+
+        Adds the current user to the context.
+
+        Args:
+            kwargs: Keyword arguments.
+
+        Returns:
+            dict: Extra context data.
+        """
         context = super().get_context_data(**kwargs)
         context['user'] = self.request.user
         return context
 
 
 class AddMemberView(LoginRequiredMixin, FormView):
+
+    """
+    View for adding a member to a chat room.
+
+    Inherits from Django's LoginRequiredMixin and FormView.
+
+    Attributes:
+        form_class: Form class for adding a member.
+        template_name: Template for rendering the form.
+    """
+
     form_class = AddMemberFrom
     template_name = 'users/register_or_login.html'
 
 
-    def form_valid(self, form):
+    def form_valid(self, form: forms.Form) -> HttpResponseRedirect:
+        """
+        Handle form submission.
+
+        Adds the specified user to the chat room if the current user is the owner.
+
+        Args:
+            form: Form instance.
+
+        Returns:
+            HttpResponseRedirect: Redirect to room page. 
+        """
         room_name = self.kwargs['room_name']
         room = RoomModel.objects.get(room_name=room_name)
         
@@ -80,7 +184,13 @@ class AddMemberView(LoginRequiredMixin, FormView):
         
         return redirect('chatapp:room', room_name=room_name)
 
-    def get_success_url(self):
+    def get_success_url(self) -> forms.Form:
+        """
+        Get success URL.
+
+        Returns:
+            HttpResponseRedirect: Redirect to main page.
+        """
         return redirect('chatapp:mainpage')
     
 
@@ -88,6 +198,18 @@ class AddMemberView(LoginRequiredMixin, FormView):
 
 
 class ChangeUserPassword(PasswordChangeView):
+
+    """
+    View for changing user's password.
+
+    Inherits from Django's PasswordChangeView.
+
+    Attributes:
+        form_class: Form class for changing password.
+        template_name: Template for rendering the form.
+        success_url: URL to redirect to after successful password change.
+    """
+
     form_class = ChangePasswordForm
     template_name = 'users/register_or_login.html'
     success_url = reverse_lazy('users:change_user_password_done')
